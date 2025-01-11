f3efaae7757cc51350c7a67513043587dab032ab
charan <hicharan03@gmail.com>
2025-01-11 17:41:10 +0530

    frontend after tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-6xl bg-red-400  ">
+      Hello world!
+      </h1>
+      <h2 className='bg-blue-400 text-5xl font-bold'>
+          gg
+      </h2>
+    </>
+  )
+}
+
+export default App
