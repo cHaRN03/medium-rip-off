0c6581525bc36fbe3919baac67f510101cdfc50a
charan <hicharan03@gmail.com>
2025-01-18 00:23:21 +0530

    blogs done ,finished 3/6 routes

---
+++ frontend/src/Components/BlogCard.tsx
@@ -0,0 +1,48 @@
+interface Blogcardtypes{
+    authorname:string,
+    title:string,
+    content:string,
+    date:string,
+
+
+}
+
+export const BlogCard = ({authorname,title,content,date}:Blogcardtypes) => {
+  return (
+    <div className="p-4 border-b border-slate-200 pb-4 w-screen max-w-screen-md cursor-pointer">
+        <div className="flex ">
+            <div className="flex justify-center flex-row pt-0.5 px-1 ">
+            <Avatar name={authorname}/>
+             
+
+        </div>
+        <div className="font-extralight pl-2 pt-0.5">{authorname}</div>
+
+        <div className="font-thin pl-2 text-slate-500 pt-0.5">. {date}</div>
+
+        </div>
+       
+        <div className="font-bold text-2xl pt-2"> 
+            {title}
+        </div>
+        <div className="font-thin text-md">
+            {content.length > 100? content.slice(0,200)+"...": content}
+        
+        </div>
+        <div className="opacity-60 pt-4">
+            {`${Math.ceil(content.length/100)} minutes read`}
+        </div>
+        
+    </div>
+  )
+}
+
+export function Avatar({name}:{name:string}){
+    return (
+       
+    <div className="relative inline-flex items-center justify-center w-7 h-6 overflow-hidden bg-gray-300 rounded-full dark:bg-gray-600">
+        <span className="font-xs text-gray-600 dark:text-gray-300">{name.slice(0,2)}</span>
+    </div>
+
+    )
+}
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,7 +1,31 @@
-
+import { Appbar } from "../Components/Appbar"
+import { BlogCard } from "../Components/BlogCard"
+import { useBlogs } from "../hooks"
 
 export const Blogs = () => {
-  return (
-    <div>Blogs</div>
+
+  const {loading,blogs}=useBlogs();
+  if(loading){
+    return <div>loading...</div>
+  }
+
+  return (    
+    <div>
+    <Appbar/>
+        <div className="flex justify-center">
+      
+          <div >
+            {blogs.map((par)=>{return (
+              <BlogCard 
+
+              authorname={par.author.name || "anonymous"} date="03/06/03" content={par.content}  title={par.title}/>
+              )
+            })}
+      
+        
+        
+            </div>
+        </div>
+    </div>
   )
 }
